a925f303caace3b56dd0f12182a589cb3480360c
Thang Tran <trankimthang279@gmail.com>
2022-08-09 22:34:29 +0200

    changed rendered image background

--- utils/binvox_visualization.py
+++ utils/binvox_visualization.py
@@ -15,12 +15,33 @@ def get_volume_views(volume, save_dir, n_itr, remark):
 
     volume = volume.squeeze().__ge__(0.5)
     fig = plt.figure()
-    # ax = fig.gca(projection=Axes3D.name)
     ax = fig.gca(projection='3d')
     ax.set_aspect('auto')
-    ax.voxels(volume, edgecolor="k")
+    ax.voxels(volume, facecolors="grey", edgecolors="black")
+
+    ax.axes.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.axes.xaxis.set_ticklabels([])
+    ax.axes.xaxis.set_ticks([])
+    ax.axes.xaxis.set_visible(False)
+
+    ax.axes.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.axes.yaxis.set_ticklabels([])
+    ax.axes.yaxis.set_ticks([])
+    ax.axes.yaxis.set_visible(False)
+
+    ax.axes.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.axes.zaxis.set_ticklabels([])
+    ax.axes.zaxis.set_ticks([])
+    ax.axes.zaxis.set_visible(False)
+
+    ax.axes.xaxis._axinfo["grid"]['color'] =  (1,1,1,0)
+    ax.axes.yaxis._axinfo["grid"]['color'] =  (1,1,1,0)
+    ax.axes.zaxis._axinfo["grid"]['color'] =  (1,1,1,0)
+
+    plt.grid(False)
+    plt.axis('off')
 
     save_path = os.path.join(save_dir, 'voxels-%06d_%s.png' % (n_itr, remark))
-    plt.savefig(save_path, bbox_inches='tight')
+    plt.savefig(save_path, transparent=True)
     plt.close()
     return cv2.imread(save_path)
